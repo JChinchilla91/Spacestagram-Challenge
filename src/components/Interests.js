7a5e037bd4c3da4bc238327d155cbb9f1f4afd01
Joshua Chinchilla <Josh@Glam.lan>
2021-09-20 13:51:37 -0700

    Completed Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,8 +2,46 @@ import React from 'react';
 
 function Interests() {
     return (
-        <>
-        </>
+        <div className='left-column-container'>
+            <div className='left-column-title'>
+                <h2 className='whitetext12'>Josh's Interests</h2>
+            </div>
+            <div className='interests-container'>
+                <div className='interest-title'>
+                    
+                </div>
+                <div className='interest'>
+                       <h3 className='lightbluetext8'>General</h3>
+                       <p>	Internet, Movies, Reading, Dancing, Karaoke, Baseball,
+                            Language, Culture, History of Communism, 
+                            Philosophy, Singing/Writing Music, Running, 
+                            Finding New Food, Weight Lifting, Hiking, WWI Aviation, 
+                            Travel, Building alternate communities</p> 
+                </div>
+                <div className='interest'>
+                       <h3 className='lightbluetext8'>Music</h3>
+                       <p>	Modest Mouse, Say Anything, Born Ruffians, Das Racist, 
+                           Injury Reserve, Radiohead, Charles Mingus, 
+                           Algernon Cadwallader, Beach House, Grizzly Bear,
+                           Snowing, Jon Brion</p> 
+                </div>
+                <div className='interest'>
+                       <h3 className='lightbluetext8'>Movies</h3>
+                       <p>	Punch-Drunk Love, Her, The Shining, No Coutry for Old Men,
+                           Fargo, Eternal Sunshine</p> 
+                </div>
+                <div className='interest'>
+                       <h3 className='lightbluetext8'>Television</h3>
+                       <p>	X-Files, Twin Peaks, DS9, TNG, King of the Hill,
+                           Adventure Time</p> 
+                </div>
+                <div className='interest'>
+                       <h3 className='lightbluetext8'>Books</h3>
+                       <p>	Anything by Vonnegut, The Stranger, Notes From Underground,
+                           American Psycho, What We Talk About When We Talk About Love</p> 
+                </div>
+            </div>
+        </div>
     )
 }
 
